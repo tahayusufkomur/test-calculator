fa8d3ab5ddc417d18fac6c039b95c7c001d537bf
tkoemue <taha_yusuf.koemuer@mercedes-benz.com>
2023-03-10 00:13:07 +0100

    prod settings

--- test_calculator/settings.py
+++ test_calculator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-))sb=60f^wy_+rt!+!m3-v85@lj#n()+fs2o&z7j954dg*4atp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
